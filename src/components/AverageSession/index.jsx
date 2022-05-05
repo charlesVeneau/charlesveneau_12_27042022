9a69e8d91929814faaff9741ee5603f6b0939365
CharlesVeneau <veneauc@hotmail.fr>
2022-05-05 21:18:04 +0200

    Intensity chart  and average session chart setup

--- src/components/AverageSession/index.jsx
+++ src/components/AverageSession/index.jsx
@@ -1,15 +1,69 @@
 import styled from 'styled-components';
 import colors from '../../utils/style/color';
+import { useAxios } from '../../utils/hooks';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Brush,
+  AreaChart,
+  Area,
+  ResponsiveContainer,
+} from 'recharts';
 
 const AverageGraph = styled.div`
-  background-color: ${colors.lightGrey};
+  background-color: ${colors.secondary};
   border-radius: 5px;
   grid-column: 1;
   height: 263px;
+  color: ${colors.tertiary};
 `;
 
 function AverageSession() {
-  return <AverageGraph>Averagegraph</AverageGraph>;
+  const userId = 12;
+  const { data, isLoading, error } = useAxios(
+    `http://localhost:3000/user/${userId}/average-sessions`
+  );
+
+  if (!isLoading && !error)
+    return (
+      <AverageGraph>
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={730}
+            height={250}
+            data={data.sessions}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="day" />
+            <YAxis />
+            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="sessionLength"
+              stroke={colors.tertiary}
+              fillOpacity={0.3}
+              fill={colors.tertiary}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </AverageGraph>
+    );
 }
 
 export default AverageSession;
--- src/components/IntensityBlock/index.jsx
+++ src/components/IntensityBlock/index.jsx
@@ -1,15 +1,63 @@
 import styled from 'styled-components';
 import colors from '../../utils/style/color';
+import { useAxios } from '../../utils/hooks';
+import {
+  ResponsiveContainer,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  Radar,
+} from 'recharts';
 
 const IntensityGraph = styled.div`
-  background-color: ${colors.lightGrey};
+  background-color: ${colors.lowBlack};
   border-radius: 5px;
   grid-column: 2;
   height: 263px;
+  color: ${colors.tertiary};
 `;
 
+/**
+ * Get response from API and format to translate in french and push it to data Array
+ * @param { Object } data
+ * @returns { Array }
+ */
+
+function getFormattedPerformances(data) {
+  data.forEach((performance) => {
+    performance.kind = data.kind.toString(performance.kind);
+  });
+  console.log(data);
+}
+
 function IntensityBlock() {
-  return <IntensityGraph>IntensityGraph</IntensityGraph>;
+  const userId = 12;
+  const { data, isLoading, error } = useAxios(
+    `http://localhost:3000/user/${userId}/performance`
+  );
+  // getFormattedPerformances(data);
+  console.log(data);
+  if (!isLoading && !error)
+    return (
+      <IntensityGraph>
+        <ResponsiveContainer width="100%" height="100%">
+          <RadarChart data={data.data}>
+            <PolarGrid />
+            <PolarAngleAxis
+              dataKey="kind"
+              axisLine={false}
+              color={colors.tertiary}
+            />
+            <Radar
+              name="performance"
+              dataKey="value"
+              fill={colors.secondary}
+              fillOpacity={0.7}
+            />
+          </RadarChart>
+        </ResponsiveContainer>
+      </IntensityGraph>
+    );
 }
 
 export default IntensityBlock;
